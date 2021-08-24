7c8f26b309bf0bb6c571e4e73b7003d44489afdc
landyking <landykingdom@gmail.com>
2021-08-24 14:37:49 +0800

    use service discovery to access authorization server

--- backend/src/main/java/com/github/landyking/BackendSecurityConfiguration.java
+++ backend/src/main/java/com/github/landyking/BackendSecurityConfiguration.java
@@ -1,17 +1,23 @@
 package com.github.landyking;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BackendSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-//    String jwkSetUri;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,9 +25,13 @@ public class BackendSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 authorizeRequests.anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
-//
-//    @Bean
-//    JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-//    }
+
+    @Bean
+    JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties, LoadBalancerInterceptor loadBalancerInterceptor) {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(loadBalancerInterceptor));
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
+                .restOperations(restTemplate)
+                .build();
+    }
 }
--- frontend/src/main/java/com/github/landyking/FrontendSecurityConfiguration.java
+++ frontend/src/main/java/com/github/landyking/FrontendSecurityConfiguration.java
@@ -1,41 +1,62 @@
 package com.github.landyking;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.endpoint.DefaultClientCredentialsTokenResponseClient;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 public class FrontendSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-//    String jwkSetUri;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests ->
                 authorizeRequests.anyRequest().permitAll()
-        );
-//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
-    //
-//    @Bean
-//    JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-//    }
+    @Bean
+    JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties, LoadBalancerInterceptor loadBalancerInterceptor) {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(loadBalancerInterceptor));
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
+                .restOperations(restTemplate)
+                .build();
+    }
+
     @Bean
     OAuth2AuthorizedClientManager authorizedClientManager(
             ClientRegistrationRepository clientRegistrationRepository,
-            OAuth2AuthorizedClientService authorizedClientRepository) {
+            OAuth2AuthorizedClientService authorizedClientRepository,
+            LoadBalancerInterceptor loadBalancerInterceptor) {
 
         OAuth2AuthorizedClientProvider authorizedClientProvider =
                 OAuth2AuthorizedClientProviderBuilder.builder()
-                        .authorizationCode()
-                        .refreshToken()
-                        .clientCredentials()
+                        .clientCredentials(builder -> {
+                            RestTemplate restTemplate = new RestTemplate(Arrays.asList(
+                                    new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
+                            restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+                            restTemplate.setInterceptors(Collections.singletonList(loadBalancerInterceptor));
+                            final DefaultClientCredentialsTokenResponseClient client = new DefaultClientCredentialsTokenResponseClient();
+                            client.setRestOperations(restTemplate);
+                            builder.accessTokenResponseClient(client);
+                        })
                         .build();
         AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(
                 clientRegistrationRepository, authorizedClientRepository);
